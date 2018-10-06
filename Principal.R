744839c13187c05f5d938904af50fecfda3cc391
psBiostat <perrine.soret@gmail.com>
2018-10-06 10:34:24 +0200

    Update Principal.R

--- Principal.R
+++ Principal.R
@@ -1,7 +1,26 @@
+#packages
 library(NADA)
 library(glmnet)
 library(hdnom)
 library(bujar)
+library(parcor)
+
+#Download useless function
+source("Bujar/bstfit_PS.R")
+source("Bujar/bujar_PS.R")
+source("Bujar/predval_PS.R")
+source("Bujar/bjboost.R")
+source("Bujar/cvmboost.R")
+source("Bujar/fpartial.R")
+source("Bujar/gcv_enet.R")
+source("Bujar/important_inter.R")
+source("Bujar/interactions.R")
+source("Bujar/mypartialPlot.R")
+source("Bujar/rif.R")
+source("Bujar/vimpint.R")
+source("GaussBJ/cvGaussBJ.R")
+source("GaussBJ/GaussBJ.R")
+
 #Data
 data(Recon)
 head(Recon)
@@ -14,13 +33,17 @@ Xnoise <- matrix(rnorm(n*p, 0, 1), ncol = p)
 Xtrue <- Recon[,1:8]
 X <- as.matrix(cbind(Xtrue, Xnoise))
 Y <- Recon$AtraConc
-#We choose the detection limit at 0.05 ug/L to have 10% of left-censoring data
-Y[which(Recon$AtraConc <= 0.05)] <- 0.05
+#the detection limit is 0.05 ug/L to have 11% of left-censoring data
+#Y[which(Recon$AtraConc <= 0.05)] <- 0.05
 Cens <- Y
 Cens[Y==0.05] <- 0
 Cens[Y>0.05] <- 1
 s <- rep(0, n) 
 s[Cens == 0] <- 0.05 #Threshold variable
+
+#Return the variable to use in survival analysis
+M = max(Y) + 1 
+Z = M - Y
 
 #Fold CV
 foldid <- rep(0, n)
@@ -32,9 +55,9 @@ foldid[which(Cens == 1)] <- sample(length(which(Cens == 1))) %% 5 + 1
 #Lasso LOD
 fit_LOD = glmnet::cv.glmnet(X, Y, nfolds = 5, foldid = foldid, alpha=1)
 
-#Lasso NonParaBJ <- version modifié du package bujar
+#Lasso NonParaBJ <- modified version from bujar package
 lambda <- seq(0, 5, by = 0.2)
-fit_NonParBJ <- bujar::bujar(x = X, y = Z, cens = Cens, learner = "lasso", lambda = lambda,
+fit_NonParBJ <- bujar(x = X, y = Z, cens = Cens, learner = "lasso", lambda = lambda,
                       nfold = 5, foldid = foldid, cv = TRUE, tuning = TRUE, trace = TRUE)
 
 #Quantile regression
